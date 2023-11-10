42b678656c27392d1af5b1a912c1402cd2d4e6bd
UlugbekBaxtiyorov <147365159+UlugbekBaxtiyorov@users.noreply.github.com>
2023-11-10 11:39:20 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,6 +1,18 @@
-void problemSolution1(float consumed_water) {
-    float cost;
-    // write your code here
-
+double problemSolution1(float water) {
+    float cost=13.0;
+    int array[3]={30,20,10};
+    double cos[4]={0.4,0.12,1.4,1.5};
+    for(int count=0;count<3;count++)
+    {
+         if(water<=array[count])
+             cost+=water*cos[count];
+         else
+             cost+=cos[count]*array[count];
+        water-=array[count];
+        if(water<0)
+            break;
+    }
+    if(water>0)
+        cost+=cos[3]*water;
     return cost;
 }
