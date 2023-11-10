f9408d30a38ad1b63c008146ba94004f91a6e68a
UlugbekBaxtiyorov <147365159+UlugbekBaxtiyorov@users.noreply.github.com>
2023-11-10 15:02:06 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,7 +1,9 @@
-#include <algorithm>
 float problemSolution2(float i, float j, float k) {
-    float result;
-    result=max(i,max(j,k));
+    float result=i;
+    float array[3]={i,j,k};
+    for(int count=1;count<3;count++)
+        if(array[count]>result)
+            result=array[count];
 
     return result;
 }
