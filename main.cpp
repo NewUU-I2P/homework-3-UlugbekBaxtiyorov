da47b66bd253f782636828df54bd650389f5b996
UlugbekBaxtiyorov <147365159+UlugbekBaxtiyorov@users.noreply.github.com>
2023-11-10 12:31:52 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,14 +9,33 @@ using namespace std;
 
 int main() {
     cout << "Problem 1\n";
-    problemSolution1();
+    double x,y,z;
+    cin>>x>>y;
+    cout<<problemSolution1(55)<<endl;
+    cout<<problemSolution1(29.9)<<endl;
     cout << "Problem 2\n";
-    problemSolution2();
+    cin>>x>>y>>z;
+    cout<<"The maximum number is "<<problemSolution2(34.9,34.5,34.2)<<endl;
+    cin>>x>>y>>z;
+    cout<<"The maximum number is "<<problemSolution2(89,45,1982)<<endl;
     cout << "Problem 3\n";
-    problemSolution3();
+    char s;
+    cin>>x;
+    cin>>s;
+    cout<<problemSolution3(1.5,'M')<<endl;
+    cin>>x;
+    cin>>s;
+    cout<<problemSolution3(1.65,'F')<<endl;
     cout << "Problem 4\n";
-    problemSolution4();
+    string str;
+    cin>>str;
+    cout<<problemSolution4("FF:FF:FF:FF:FF:FF")<<endl;
+    cin>>str;
+    cout<<problemSolution4("18:20:3F:20:AB:11")<<endl;
     cout << "Problem 5\n";
-    problemSolution5();
+    cin>>x>>y>>s;
+    cout<<problemSolution5(x,y,s)<<endl;
+    cin>>x>>y>>s;
+    cout<<problemSolution5(x,y,s)<<endl;
     return 0;
 }
