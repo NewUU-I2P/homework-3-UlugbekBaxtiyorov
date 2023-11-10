fa35049f64547ffb0c1a3a9f2be00a61b16d6404
UlugbekBaxtiyorov <147365159+UlugbekBaxtiyorov@users.noreply.github.com>
2023-11-10 12:04:30 +0500

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -1,9 +1,30 @@
 #include <string>
 #include <sstream>
-
-std::string problemSolution4(const std::string &macAddress) {
-    // write your code here
-
-    // make use of control flow statements
-    // return result;
+#include <cctype>
+using namespace std;
+string problemSolution4(const string &macAddress) 
+{
+    string str="";
+    int array[6]={10,11,12,13,14,15};
+    str+=macAddress[0];
+    str+=macAddress[1];
+    if(str=="FF")
+        return "Broadcast";
+    if(isdigit(str[1]))
+    {
+        int num=str[1]-'0';
+        if(num%2==0)
+            return "Unicast";
+        else
+            return "Multicast";
+    }
+    else
+    {
+        int num=array[str[1]-'A'];
+        if(num%2==0)
+            return "Unicast";
+        else
+            return "Multicast";
+    }
+    return "Unicast";
 }
